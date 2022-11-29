08689eefe56efa419ae8c56143acd4e06c374c29
Yeonguk Kim <rudghk1437@naver.com>
2022-11-29 11:21:22 +0900

    community앱, Article 모델 생성, 글작성 폼 만들기

---
+++ community/forms.py
@@ -0,0 +1,6 @@
+from django.forms import ModelForm
+from community.models import Article
+class Form(ModelForm):
+  class Meta:
+    model = Article
+    fields = ['name', 'title', 'contents', 'url', 'email']
--- community/views.py
+++ community/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-
+from .forms import Form
 # Create your views here.
 def write(request):
+  # request의 post true이면
+  # 사용자가 입력한 form 데이터를 변수에 저장
+  # ORM으로 DB에 저장하기
+  if request.method == 'POST':
+    form = Form(request.POST)
+    if form.is_valid():
+      form.save() # 필드값 저장함.
+  else:
+    form =Form()
+  return render(request, 'write.html', {'form': form}) 
   # 비즈니스 로직 구현
   # data ={'키':'값'}
   # return render(request, 'html템플릿 파일.html',data)
-  hello = "헬로 django"
-  hello2 = "알고보니 쉬운 장고"
-  return render(request, 'write.html', {'data': hello, 'data2':hello2})
+
+  # return render(request, 'write.html', {'data': hello, 'data2':hello2})
