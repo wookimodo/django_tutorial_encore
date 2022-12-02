49c5f4a41857885dc1ec9debc6381d19b05da0dd
Yeonguk Kim <rudghk1437@naver.com>
2022-12-02 10:48:45 +0900

    community앱 클래스형 뷰로 변경

--- community/urls.py
+++ community/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from community.views import write, articleList, viewDetail
+from community import views
 
 app_name = 'community'
 
 urlpatterns = [
-    path('write/', write, name='write'),
-    path('list/', articleList, name='list'),
-    path('view_detail/<int:num>/', viewDetail, name='view_detail'),
+    # /write/
+    path('write/', views.WriteFormView.as_view(), name='write'),
+    # path('write/', write, name='write'),
+    # /list/
+    path('list/', views.ArticleListView.as_view(), name='list'),
+    # path('list/', views.articleList, name='list'),
+    # /view_detail/1/
+    path('view_detail/<slug:pk>/', views.ArticleDetailView.as_view(), name='view_detail'),
+    # path('view_detail/<int:num>/', views.viewDetail, name='view_detail'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,34 +1,53 @@
 from django.shortcuts import render
 from .forms import Form
 from .models import Article
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView
+
 # Create your views here.
-def write(request):
-  # request의 post true이면
-  # 사용자가 입력한 form 데이터를 변수에 저장
-  # ORM으로 DB에 저장하기
-  if request.method == 'POST':
-    form = Form(request.POST)
-    if form.is_valid():
-      form.save() # 필드값 저장함.
-  else:
-    form = Form()
-  return render(request, 'community/write.html', {'form': form}) 
-  # 비즈니스 로직 구현
-  # data ={'키':'값'}
-  # return render(request, 'html템플릿 파일.html',data)
+# def write(request):
+#   # request의 post true이면
+#   # 사용자가 입력한 form 데이터를 변수에 저장
+#   # ORM으로 DB에 저장하기
+#   if request.method == 'POST':
+#     form = Form(request.POST)
+#     if form.is_valid():
+#       form.save() # 필드값 저장함.
+#   else:
+#     form = Form()
+#   return render(request, 'community/write.html', {'form': form}) 
+#   # 비즈니스 로직 구현
+#   # data ={'키':'값'}
+#   # return render(request, 'html템플릿 파일.html',data)
 
-  # return render(request, 'write.html', {'data': hello, 'data2':hello2})
-def articleList(request):
-  article_list = Article.objects.all()
-  return render(request, 'community/list.html', {'article_list': article_list})
+#   # return render(request, 'write.html', {'data': hello, 'data2':hello2})
+# def articleList(request):
+#   article_list = Article.objects.all()
+#   return render(request, 'community/list.html', {'article_list': article_list})
 
-def viewDetail(request, num=1):
-  # 클릭한 레코드를 DB 읽어오기 
-  article_detail = Article.objects.get(id=num)
-  # article_detail = get_object_or_404(Article, id=num)
-  return render(request, 'community/view_detail.html', {'article_detail': article_detail})
+# def viewDetail(request, num=1):
+#   # 클릭한 레코드를 DB 읽어오기 
+#   article_detail = Article.objects.get(id=num)
+#   # article_detail = get_object_or_404(Article, id=num)
+#   return render(request, 'community/view_detail.html', {'article_detail': article_detail})
 
 def index(request):
   article_len = len(Article.objects.all())
   latest_article_list = Article.objects.all().order_by('cdate')[article_len-3:article_len]
   return render(request, 'index.html', {'latest_article_list': latest_article_list})
+
+class WriteFormView(CreateView):
+  model = Article
+  fields = ['name', 'title', 'contents', 'url', 'email']
+  template_name = 'community/write.html'
+  success_url = reverse_lazy('community:article_list')
+  def form_valid(self, form):
+    return super().form_valid(form)
+  
+class ArticleListView(ListView):
+  model = Article
+  template_name = 'community/list.html'
+  
+class ArticleDetailView(DetailView):
+  model = Article
+  template_name = 'community/view_detail.html'
